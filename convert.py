c2f9b3aded4b35b59f60372697d1c04cbe1e2385
Fahimul Hoque <fahimul.shubho@northsouth.edu>
2021-02-09 22:46:06 +0600

    :export list of invalid xml files

--- convert.py
+++ convert.py
@@ -25,44 +25,57 @@ xml_files = glob.glob(path_1)
 
 total = len(xml_files)
 total_count = 0
+invalid_files = []
 
 print('Converting...')
 for files in xml_files:
     #print(files)
-    root = ET.parse(files).getroot()
+    try: 
+        tree = root = ET.parse(files).getroot()
 
-    f_name = os.path.basename(files)
-    output = f_name[:-3]
-    output = output + 'txt'
-    #print(output)
+        f_name = os.path.basename(files)
+        output = f_name[:-3]
+        output = output + 'txt'
+        #print(output)
 
-    size = root.find('size')
-    #print(float(size[0].text))
-    width = float(size[0].text)
-    height = float(size[1].text)
+        size = root.find('size')
+        #print(float(size[0].text))
+        width = float(size[0].text)
+        height = float(size[1].text)
 
-    #conversion to YOLO format
-    for objects in root.findall('object'):
-        class_name = objects.find('name').text
-        class_label = names[class_name]
-        #print(class_label)
+        #conversion to YOLO format
+        for objects in root.findall('object'):
+            class_name = objects.find('name').text
+            class_label = names[class_name]
+            #print(class_label)
 
-        box = objects.find('bndbox')
-        xmin = float(box[0].text)
-        ymin = float(box[1].text)
-        xmax = float(box[2].text)
-        ymax = float(box[0].text)
+            box = objects.find('bndbox')
+            xmin = float(box[0].text)
+            ymin = float(box[1].text)
+            xmax = float(box[2].text)
+            ymax = float(box[0].text)
 
-        x = (xmax + xmin) / (width * 2)
-        y = (ymax + ymin) / (height * 2)
-        w = (xmax - xmin) / width
-        h = (ymax - ymin) / height
+            x = (xmax + xmin) / (width * 2)
+            y = (ymax + ymin) / (height * 2)
+            w = (xmax - xmin) / width
+            h = (ymax - ymin) / height
 
-        #writing to text file
-        with open(path + '\\' + output, 'a+') as output_file:
-            output_file.write(' '.join([str(class_label), str(x), str(y), str(w), str(h) + '\n']))
+            #writing to text file
+            with open(path + '\\' + output, 'a+') as output_file:
+                output_file.write(' '.join([str(class_label), str(x), str(y), str(w), str(h) + '\n']))
     
-    total_count += 1
-    print(total_count, 'converted out of', total)
+        total_count += 1
+        print(total_count, 'converted out of', total)
 
-print('Completed')
+    except:
+        print(os.path.basename(files), 'Invalid XML file')
+        invalid_files.append(os.path.basename(files)+'\n')
+
+
+with open(path + '\\' + 'invalid_files.txt', 'a+') as output_file:
+    output_file.write(invalid_files)
+    
+print('\nCompleted')
+print(total_count, 'files converted successfully\n')
+print(total - total_count, 'ivalid files')
+print('See invalid file list in invalid_files.txt')
